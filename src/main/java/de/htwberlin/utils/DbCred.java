7f81a0c2e3634630180a496ae79ad250412d477b
TOS <ostermaier.timm@gmail.com>
2018-12-28 16:11:22 +0100

    ADD PLSQL implementation

--- src/main/java/de/htwberlin/utils/DbCred.java
+++ src/main/java/de/htwberlin/utils/DbCred.java
@@ -1,9 +1,9 @@
 package de.htwberlin.utils;
 
 public interface DbCred {
-	  final String driverClass = "oracle.jdbc.driver.OracleDriver";
-	  final String url = "jdbc:oracle:thin:@xxx";
-	  final String user = "uxxx";
-	  final String password = "pxxx";
-	  final String schema = "uxxx";
+	final String driverClass = "oracle.jdbc.driver.OracleDriver";
+	final String url = "jdbc:oracle:thin:@oradbs02.f4.htw-berlin.de:1521:oradb1";
+	final String user = "u563963";
+	final String password = "p563963";
+	final String schema = "u563963";
 }
