d1de6d34cc5d1ec5bd495f6b538833b6cee7fb37
FabianFra <33935010+FabianFra@users.noreply.github.com>
2018-12-31 15:52:03 +0100

    Function helper
    added a description for writing pl / sql functions and how to debug code

--- src/main/plsql script/script.sql
+++ src/main/plsql script/script.sql
@@ -1,48 +1,67 @@
-CREATE OR REPLACE PACKAGE BODY maut_service 
+/*Bitte achte drauf, dass die DBMS-Ausgabe aktiviert ist. (Ansicht -> DBMS-Ausgabe)*/
+SET SERVEROUTPUT ON;
+
+CREATE OR REPLACE PACKAGE BODY maut_service IS
+    
+    /*
+    Dies ist eine Beispielfunktion, in welcher du die grobe Synthax sehen kannst. Nehme das einfach als Muster für die 
+    zu schreibenden Funktionen. Falls du eine neue Funktion hinzufügst oder diese veränderst musst du dieses Script erneut
+    ausführen.
+    */
+    FUNCTION TestFunktion(p_name IN VARCHAR2)
+    RETURN VARCHAR2
+    IS
+    BEGIN
+    RETURN('WELCOME ' || p_name);
+    END test123;
 
 
-AS PROCEDURE BERECHNEMAUT(P_MAUTABSCHNITT MAUTABSCHNITT.ABSCHNITTS_ID%TYPE, P_ACHSZAHL FAHRZEUG.ACHSEN%TYPE, P_KENNZEICHEN FAHRZEUG.KENNZEICHEN%TYPE) AS 
-v_dummy integer; 
-fID  FAHRZEUG.FZ_ID%TYPE;
-achs  FAHRZEUG.ACHSEN%TYPE;
-kz FAHRZEUG.KENNZEICHEN%TYPE;
-aut BOOLEAN;
-BEGIN 
+
+    PROCEDURE BERECHNEMAUT(P_MAUTABSCHNITT MAUTABSCHNITT.ABSCHNITTS_ID%TYPE, P_ACHSZAHL FAHRZEUG.ACHSEN%TYPE, P_KENNZEICHEN FAHRZEUG.KENNZEICHEN%TYPE) AS 
+    v_dummy integer; 
+    fID  FAHRZEUG.FZ_ID%TYPE;
+    achs  FAHRZEUG.ACHSEN%TYPE;
+    kz FAHRZEUG.KENNZEICHEN%TYPE;
+    aut BOOLEAN;
 
 
+    BEGIN 
+    DBMS_OUTPUT.PUT_LINE(TestFunktion('Fabian')); /*So schreibst du etwas in die DBMS-Ausgabe*/
+    
+    
+    
+    SELECT f.KENNZEICHEN, f.ACHSEN
+    into kz, achs
+    FROM Fahrzeug f
+    WHERE f.KENNZEICHEN = P_KENNZEICHEN;
 
-SELECT f.KENNZEICHEN, f.ACHSEN
-into kz, achs
-FROM Fahrzeug f
-WHERE f.KENNZEICHEN = P_KENNZEICHEN;
+    SELECT b.KENNZEICHEN into kz
+    FROM BUCHUNG b
+    WHERE b.KENNZEICHEN = P_KENNZEICHEN;
 
-SELECT b.KENNZEICHEN into kz
-FROM BUCHUNG b
-WHERE b.KENNZEICHEN = P_KENNZEICHEN;
-
-IF kz IS NOT NULL  THEN
-    IF achs != P_ACHSZAHL THEN
+    IF kz IS NOT NULL  THEN
+        IF achs != P_ACHSZAHL THEN
+            raise INVALID_VEHICLE_DATA;
+        ELSE 
         raise INVALID_VEHICLE_DATA;
-    ELSE 
-     raise INVALID_VEHICLE_DATA;
 
-END IF;
+    END IF;
 /*
 manuel
 */
 
 
-END IF;
+    END IF;
 
-EXCEPTION 
-WHEN NO_DATA_FOUND then
-  raise UNKOWN_VEHICLE;
-WHEN others then
-   raise UNKOWN_VEHICLE;
+    EXCEPTION 
+    WHEN NO_DATA_FOUND then
+        raise UNKOWN_VEHICLE;
+    WHEN others then
+        raise UNKOWN_VEHICLE;
    
         
     
             
-END BERECHNEMAUT;
+    END BERECHNEMAUT;
 
 END maut_service;
