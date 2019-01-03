70e30f5c4535f9e7b38353acc59d0100248b60b5
TOS <ostermaier.timm@gmail.com>
2019-01-03 19:49:01 +0100

    add basic function

--- src/main/plsql script/script.sql
+++ src/main/plsql script/script.sql
@@ -13,9 +13,67 @@ CREATE OR REPLACE PACKAGE BODY maut_service IS
     IS
     BEGIN
     RETURN('WELCOME ' || p_name);
-    END test123;
+    END TestFunktion;
 
 
+  FUNCTION FindFahrzeugInBuchungTB(p_kennzeichen IN VARCHAR2)
+    RETURN NUMBER
+    IS achs VARCHAR2(5);
+    BEGIN
+        SELECT  m.ACHSZAHL
+        into  achs
+        FROM Buchung b
+        INNER JOIN MAUTKATEGORIE m
+        ON b.KATEGORIE_ID = m.KATEGORIE_ID
+        WHERE B.KENNZEICHEN = P_KENNZEICHEN;
+              
+        return 2;
+        EXCEPTION 
+            WHEN NO_DATA_FOUND then
+            raise UNKOWN_VEHICLE;
+            
+  
+    END FindFahrzeugInBuchungTB;
+    
+    FUNCTION FindFahrzeugInFahrzeugTB(p_kennzeichen IN VARCHAR2)  
+    RETURN NUMBER
+    IS achs NUMBER;
+    BEGIN 
+        
+        SELECT  f.ACHSEN
+        into  achs
+        FROM Fahrzeug f
+        WHERE f.KENNZEICHEN = P_KENNZEICHEN;
+    
+        return achs;
+    
+        EXCEPTION 
+            WHEN NO_DATA_FOUND then
+                return FindFahrzeugInBuchungTB(p_kennzeichen);   
+        
+            
+        
+    END FindFahrzeugInFahrzeugTB;
+    
+    FUNCTION PruefeAchsAnzahl(p_achs IN NUMBER ,f_achs IN NUMBER)
+    RETURN boolean
+    IS
+    BEGIN 
+        IF p_achs = f_achs THEN
+            return true;
+        ELSE
+              
+               return false;
+        END IF;
+        
+    END PruefeAchsAnzahl;
+    
+    
+    
+    
+  
+    
+    
 
     PROCEDURE BERECHNEMAUT(P_MAUTABSCHNITT MAUTABSCHNITT.ABSCHNITTS_ID%TYPE, P_ACHSZAHL FAHRZEUG.ACHSEN%TYPE, P_KENNZEICHEN FAHRZEUG.KENNZEICHEN%TYPE) AS 
     v_dummy integer; 
@@ -24,13 +82,17 @@ CREATE OR REPLACE PACKAGE BODY maut_service IS
     kz FAHRZEUG.KENNZEICHEN%TYPE;
     aut BOOLEAN;
 
-
     BEGIN 
-    DBMS_OUTPUT.PUT_LINE(TestFunktion('Fabian')); /*So schreibst du etwas in die DBMS-Ausgabe*/
+  /*   DBMS_OUTPUT.PUT_LINE(TestFunktion('Fabian'));So schreibst du etwas in die DBMS-Ausgabe*/
+    achs := FindFahrzeugInFahrzeugTB(P_KENNZEICHEN);
+    if PruefeAchsAnzahl(achs,P_ACHSZAHL) = TRUE then
+        DBMS_OUTPUT.PUT_LINE('Hallo');
+    else
+     raise INVALID_VEHICLE_DATA ;
+    END IF;
+    /*DBMS_OUTPUT.PUT_LINE(FindFahrzeugInFahrzeugTB(P_KENNZEICHEN)); */
     
-    
-    
-    SELECT f.KENNZEICHEN, f.ACHSEN
+  /*  SELECT f.KENNZEICHEN, f.ACHSEN
     into kz, achs
     FROM Fahrzeug f
     WHERE f.KENNZEICHEN = P_KENNZEICHEN;
@@ -49,7 +111,7 @@ CREATE OR REPLACE PACKAGE BODY maut_service IS
 /*
 manuel
 */
-
+/*
 
     END IF;
 
@@ -58,7 +120,7 @@ manuel
         raise UNKOWN_VEHICLE;
     WHEN others then
         raise UNKOWN_VEHICLE;
-   
+   */
         
     
             
